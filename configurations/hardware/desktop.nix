6724e04e8e0b513d0338d68ed71b718be04be9f1
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-10-30 15:22:19 -0300

    ~

--- configurations/hardware/desktop.nix
+++ configurations/hardware/desktop.nix
@@ -6,7 +6,7 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
