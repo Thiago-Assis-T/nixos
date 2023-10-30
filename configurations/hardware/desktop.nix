e603feface5771494687d5d7cd92be6174cc02a0
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-10-30 14:57:57 -0300

    ~

--- configurations/hardware/desktop.nix
+++ configurations/hardware/desktop.nix
@@ -4,29 +4,28 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "rtl8188eu" "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/63e8dbc8-6720-40b8-8ee4-7ce1e5f3380d";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/63e8dbc8-6720-40b8-8ee4-7ce1e5f3380d";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1415-ECF7";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/1415-ECF7";
+    fsType = "vfat";
+  };
 
-  fileSystems."/home/thiago/.moneroNode" =
-    { device = "/dev/disk/by-uuid/e1f131f8-83eb-4c06-962e-b4fe7cc1f545";
-      fsType = "ext4";
-    };
+  fileSystems."/home/thiago/.moneroNode" = {
+    device = "/dev/disk/by-uuid/e1f131f8-83eb-4c06-962e-b4fe7cc1f545";
+    fsType = "ext4";
+  };
 
   swapDevices = [ ];
 
