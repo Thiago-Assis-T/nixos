ec43e15e451ec2c9aba0766da3577438f5e76f20
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-10-31 20:37:34 -0300

    ~

--- home/modules/nvim.nix
+++ home/modules/nvim.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, unstable, ... }: {
+{ config, pkgs, ... }: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
@@ -235,7 +235,7 @@
     '';
 
     extraPackages = with pkgs; [
-      unstable.nixd
+      nixd
       ripgrep
       fd
       nodejs
