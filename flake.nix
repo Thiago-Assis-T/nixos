4d44e36514799153cd5c408c3875190ccdca3db4
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-10-31 20:26:34 -0300

    ~

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager"; # /release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
