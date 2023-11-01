1414830400a5691b7ff22e4703a5e4073c7f89e2
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-11-01 20:12:20 -0300

    going to foot out off wezterm, cause it broke

--- home/home.nix
+++ home/home.nix
@@ -8,14 +8,14 @@
     ./modules/nvim.nix
     ./modules/hyprland.nix
     ./modules/browser.nix
-    ./modules/wezterm.nix
+    ./modules/foot.nix
   ];
   fonts.fontconfig.enable = true;
   home = {
     username = "thiago";
     homeDirectory = "/home/thiago";
     stateVersion = "22.11";
-    packages = with pkgs; [ htop monero-gui foot ];
+    packages = with pkgs; [ htop monero-gui ];
   };
 
   qt = {
