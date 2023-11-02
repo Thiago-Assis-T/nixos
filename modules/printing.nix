4abb841a05972a71031348683a9afa79e178533d
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-11-02 14:11:03 -0300

    fixing stuff

--- modules/printing.nix
+++ modules/printing.nix
@@ -24,7 +24,7 @@
 
   hardware.printers = {
     ensurePrinters = [{
-      name = "Epson L3150";
+      name = "Epson_L3150_Thiago";
       location = "Thiago";
       deviceUri =
         "usb://EPSON/L3150%20Series?serial=583545564232303674&interface=1";
