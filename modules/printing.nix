8ce56837ef4184beb6961e7b839ed0dcb09c5f2d
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-11-02 14:08:54 -0300

    declaring the printer

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ system-config-printer ];
   services.printing = {
     enable = true;
     drivers = with pkgs; [ epson-escpr epson-escpr2 ];
@@ -9,7 +8,6 @@
     browsing = true;
     defaultShared = true;
   };
-
   services.avahi = {
     enable = true;
     nssmdns = true;
@@ -24,4 +22,15 @@
     allowedUDPPorts = [ 631 ];
   };
 
+  hardware.printers = {
+    ensurePrinters = [{
+      name = "Epson L3150";
+      location = "Thiago";
+      deviceUri =
+        "usb://EPSON/L3150%20Series?serial=583545564232303674&interface=1";
+      model = "EPSON L3150 Series";
+      ppdOptions = { PageSize = "A4"; };
+    }];
+  };
+  environment.systemPackages = with pkgs; [ system-config-printer ];
 }
