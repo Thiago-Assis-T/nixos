cdbdf94135c78025cfbd544315ba9bf31ae4b1c6
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-11-02 14:05:20 -0300

    adding the sharing capability

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,8 +1,27 @@
 { config, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ system-config-printer ];
   services.printing = {
     enable = true;
     drivers = with pkgs; [ epson-escpr epson-escpr2 ];
+
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
+    browsing = true;
+    defaultShared = true;
   };
-  environment.systemPackages = with pkgs; [ system-config-printer ];
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+  networking.firewall = {
+    allowedTCPPorts = [ 631 ];
+    allowedUDPPorts = [ 631 ];
+  };
 
 }
