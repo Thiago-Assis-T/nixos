0d0208582c192ff8658cda10326127b44ca3fe09
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-11-06 22:12:36 -0300

    printer sharing atempt

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,4 +1,28 @@
 { config, pkgs, ... }: {
+  services.samba = {
+    enable = true;
+    package = pkgs.sambaFull;
+    openFirewall = true; # Automatically open firewall ports
+    extraConfig = ''
+      load printers = yes
+      printing = cups
+      printcap name = cups
+    '';
+    shares = {
+      printers = {
+        comment = "All Printers";
+        path = "/var/spool/samba";
+        public = "yes";
+        browseable = "yes";
+        # to allow user 'guest account' to print.
+        "guest ok" = "yes";
+        writable = "no";
+        printable = "yes";
+        "create mode" = 700;
+      };
+    };
+  };
+  systemd.tmpfiles.rules = [ "d /var/spool/samba 1777 root root -" ];
   services.printing = {
     enable = true;
     drivers = with pkgs; [ epson-escpr epson-escpr2 ];
