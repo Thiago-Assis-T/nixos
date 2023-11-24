32dc95860b0d857a1da8196ec44bea7605a1537f
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-11-24 14:11:23 -0300

    stuff

--- home/home.nix
+++ home/home.nix
@@ -1,4 +1,4 @@
-{ unstable-pkgs, inputs, config, pkgs, ... }:
+{ unstable-pkgs, inputs, config, pkgs, lib, ... }:
 
 {
   imports = [
@@ -21,7 +21,21 @@
       unstable-pkgs.zoom-us
       bat
     ];
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      QT_QPA_PLATFORM = "wayland";
+      GDK_BACKEND = "wayland";
+      SDL_VIDEODRIVER = "wayland";
+      CLUTTER_BACKEND = "wayland";
+      XDG_SESSION_TYPE = "wayland";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      XCURSOR_SIZE = "20";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      QT_QPA_PLATFORMTHEME = lib.mkForce "gtk";
+    };
   };
+
   services.gnome-keyring.enable = true;
   xdg.enable = true;
 }
