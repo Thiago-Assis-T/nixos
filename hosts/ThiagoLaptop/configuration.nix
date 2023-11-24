db85f54b5d4053c5f94a7f750fe0b60cc8d63680
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-11-24 17:53:08 -0300

    stuff

--- hosts/ThiagoLaptop/configuration.nix
+++ hosts/ThiagoLaptop/configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
-    ../../modules/games.nix
+    # ../../modules/games.nix
     ../../modules/networking.nix
     ../../modules/nix.nix
     ../../modules/services.nix
