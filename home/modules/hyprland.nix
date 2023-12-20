003ba99675db76fe51c664f8662ea8705623c293
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-12-20 16:58:43 -0300

    mouse scroll fix

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -41,7 +41,7 @@
           follow_mouse = 1
 
           touchpad {
-              natural_scroll = false
+              natural_scroll = true 
           }
 
           sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
