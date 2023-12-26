ae033d8b434e7d2092f8e06c1ff3c41114dc6c2d
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-12-26 00:23:40 -0300

    removing unused steam configs

--- modules/games.nix
+++ modules/games.nix
@@ -3,9 +3,9 @@
     gamemode.enable = true;
     steam = {
       enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      gamescopeSession.enable = true;
+      remotePlay.openFirewall = false;
+      dedicatedServer.openFirewall = false;
+      gamescopeSession.enable = false;
       extraCompatPackages =
         [ inputs.nix-gaming.packages.${pkgs.system}.proton-ge ];
     };
