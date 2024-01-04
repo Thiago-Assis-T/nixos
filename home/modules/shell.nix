471bd241e7727bb5b8ec555c74565aa9fa7b3fda
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-01-04 20:30:50 -0300

    tmux and alias for nvim to fuzzy find

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -6,7 +6,14 @@
       shellAliases = {
         htop = "sudo htop";
         cat = "bat";
+        nvim = "nvim $(fzf)";
+        vim = "nvim $(fzf)";
+        vi = "nvim $(fzf)";
       };
+      bashrcExtra = ''
+        if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+            tmux attach -t default || tmux new -s default
+        fi'';
     };
     starship = {
       enable = true;
