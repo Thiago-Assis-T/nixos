67b5269b311bc7eba6523abfc14e67b5ec1fb35c
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-01-04 23:10:25 -0300

    added a tor relay setup, cause why not

--- modules/networking.nix
+++ modules/networking.nix
@@ -21,31 +21,49 @@
     };
   };
 
-  services.dnscrypt-proxy2 = {
-    enable = true;
-    settings = {
-      ipv4_servers = true;
-      ipv6_servers = true;
-      dnscrypt_servers = true;
-      doh_servers = true;
-      odoh_servers = true;
+  services = {
 
-      require_dnssec = true;
-      require_nolog = true;
-      require_nofilter = true;
+    tor = {
+      enable = true;
+      openFirewall = true;
+      relay = {
+        enable = true;
+        role = "relay";
+      };
+      settings = {
+        # ContactInfo = "toradmin@example.org";
+        # Nickname = "toradmin";
+        ORPort = 9001;
+        ControlPort = 9051;
+        # BandWidthRate = "1 MBytes";
+      };
+    };
+    dnscrypt-proxy2 = {
+      enable = true;
+      settings = {
+        ipv4_servers = true;
+        ipv6_servers = true;
+        dnscrypt_servers = true;
+        doh_servers = true;
+        odoh_servers = true;
 
-      force_tcp = false;
+        require_dnssec = true;
+        require_nolog = true;
+        require_nofilter = true;
 
-      listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
-      max_clients = 250;
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-        minisign_key =
-          "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        force_tcp = false;
+
+        listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+        max_clients = 250;
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          minisign_key =
+            "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
       };
     };
   };
